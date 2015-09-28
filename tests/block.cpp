9e42eb97d526736f6ff946b37ae25fcd737b9091
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-09-28 19:01:19 +0200

    Adds test for Block << Operator

--- tests/block.cpp
+++ tests/block.cpp
@@ -1,5 +1,19 @@
 #include <gtest/gtest.h>
 
-#include "block.hpp"
+#include <blk.hpp>
+
+
+#include "utils.hpp"
+
+
+TEST(Block, Output) {
+    blk::Expr e = blk::debug('a', 1, 1, 0, 0);
+
+    std::ostringstream output;
+
+    output << *e;
+
+    ASSERT_EQ("    0 :  +\n", output.str()); 
+}
 
 
