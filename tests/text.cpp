9360f203cc94b5cc0652c97c709097c473488aa0
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-09-28 17:41:22 +0200

    Adds test for Text Block

---
+++ tests/text.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "blk.hpp"
+
+
+TEST(Text, Test)
+{
+    blk::Expr t1 = blk::text("hello", blk::Text::Alignment::Left);
+    blk::Expr t2 = blk::text("hello", blk::Text::Alignment::Center);
+    blk::Expr t3 = blk::text("coucou", blk::Text::Alignment::Center);
+    blk::Expr t4 = blk::text("hello", blk::Text::Alignment::Right);
+
+    ASSERT_EQ(0, t1->column());
+    ASSERT_EQ(2, t2->column());
+    ASSERT_EQ(2, t3->column());
+    ASSERT_EQ(4, t4->column());
+
+}
